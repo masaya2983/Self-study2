25503abc5697c613087141fb89ff953b1e5d0b51
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-03 00:44:54 +0000

    2週目

---
+++ 6章/assosiative_in_assosiative.php
@@ -0,0 +1,12 @@
+<pre>
+  <?php
+  //テスト結果を連想配列にする
+  $result = array("math" => 90, "English" => 80);
+  //連想配列$friendsにテスト結果の連想配列を代入する
+  $friends = array("Haruki" => $result);
+  var_dump($friends);//結果を確認
+  
+  //連想配列にキーを指定して追加し、値として連想配列を代入
+  $friends["Kaoru"] = array("math" => 95,"Englisgh" =)
+  ?>
+</pre>
