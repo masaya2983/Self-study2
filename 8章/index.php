9825e71988f30e34fa97267a0105fa029733cb31
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-27 10:29:28 +0000

    8-1

--- 8章/index.php
+++ 8章/index.php
@@ -15,8 +15,7 @@
       </div>
     </main>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
-    <script>window.jQuery|| document.write('<script src="/docs/4.5/assets/js/vendar/jquery-slim.min.js"><\/script'>)
-    </script>
-    <script src="https://stac"
+    <script>window.jQuery|| document.write('<script src="/docs/4.5/assets/js/vendar/jquery-slim.min.js"><\/script'>)</script>
+    <script src="https://stacpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
