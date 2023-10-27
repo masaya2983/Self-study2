09b71ad6beecaf9b68778e8654eed94a59987e75
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-27 15:11:59 +0000

    8-1

---
+++ 8章/navbar.php
@@ -0,0 +1,11 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-datk fixed-top">
+  <a class="navbar-brand" href="./index.php">サークルサイト</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="collapse" data-target="#navbarsExampleDefult" aria-controls="navbarsExampleDefult" aria-controls "navbarsExampleDefult" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="callapse navbar-collapse"id="navbarsExampleDefult">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item"><a class = "nav-link" </li>
+    </ul>
+  </div>
+</nav>
