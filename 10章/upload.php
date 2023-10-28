b01f535c0e8ebc6890b4cb16bfe64d6e0913698d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-28 11:54:06 +0000

    10-4

---
+++ 10章/upload.php
@@ -0,0 +1,8 @@
+<h1>画像をアップロード</h1>
+<form action="upload.php" method="post" enctype="multipart/form-date">
+  <div class="form-group">
+    <label>アップロードファイル</label>
+    <input type="file" name="image" class="form-control-file">
+  </div>
+  <input type="submit" value="アップロードする" class="btn btn-primary">
+</form>
