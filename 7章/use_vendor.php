85efcd51a435ee730a0cdf1e4e5bf7bdfd79b5f6
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-28 03:18:18 +0000

    9-4

--- 7章/use_vendor.php
+++ 7章/use_vendor.php
@@ -4,5 +4,5 @@ include 'vendor.php';
 echo vending_machine(120,"オレンジジュース");
 $price = 90;
 $juice_name = "アップルジュース";
-echo vending_machine($price,$juice_name)
+echo vending_machine($price,$juice_name);
 ?>
--- 9章/index.php
+++ 9章/index.php
@@ -1,7 +1,24 @@
 <?php $info = file_get_contents("info.txt") ?>
 <!doctype html>
-
+<html lang="ja"></html>
 
 
 <h1>お知らせ</h1>
-<p><?php echo $info; </p>
+<p><?php 
+if ($fp){
+  
+  $title = fgets($fp);
+  if ($title){
+    
+    echo '<p><a href="info.php">'.$title.'</a></p>';
+  }else{
+    echo '<p>お知らせはありません</p>';
+  }
+  fclose($$fp);
+}else{
+  
+
+echo '<p> お知らせはありません</p>';
+}
+
+?>
