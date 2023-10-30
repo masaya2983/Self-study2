4b5e8c4d409bd6d09599eb114154fa5df7af1923
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-30 10:53:22 +0000

    11-1

---
+++ 11章/navbar.php
@@ -0,0 +1,4 @@
+<li class ="nav-item"><a class="nav-link" href="info.php">お知らせ</a></li>
+<li class ="nav-item"><a class="nav-link" href="upload.php">画像をアップロード</a></li>
+<li class ="nav-item"><a class="nav-link" href="album.php">アルバム</a></li>
+<li class ="nav-item"><a class="nav-link" href="bbs.php">掲示板</a></li>
