24ca3e4d965cb9e1895db5b20a4c1b35d46b23c4
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-31 03:59:09 +0000

    11-1

---
+++ 11章/write.php
@@ -0,0 +1,39 @@
+<?php
+$naame = $_POST['name'];
+$title = $_POST['title'];
+$body = $_POST['body'];
+$pass = $_POST['pass'];
+
+
+if ($name == ''|| $body == ''){
+  header("Location: bbs.php");
+  exit();
+}
+if (!preg_match("/^[0-9]{4}$/",$pass)){
+  header("Location: bbs.php");
+  exit();
+}
+$dsn = 'mysql:host=localhost;dbname=tennis;charset=utf8';
+$user = 'tennisuser';
+$password = 'password';
+try{
+  
+  $db = new PDO($dsn,$user,$password);
+  $db->setAttribute(PDO::ATTR_EMLATE_PREPARSE, false);
+  
+  $stmt = $db->prepare("INSERT INTO bbs(name, title, body, date,pass)
+  Values(:name, ;title, :body, :now(), :pass)");
+  
+  $stmt->bindParm(':name', $name, PDO::PARSM_STR);
+  $stmt->bindParm(':title', $title, PDO::PARSM_STR);
+  $stmt->bindParm(':body', $body, PDO::PARSM_STR);
+  $stmt->bindParm(':pass', $pass, PDO::PARSM_STR);
+  
+  $stmt->excute();
+  
+  header('Location:bbs.php');
+  exit();
+} catch(PDOException $e){
+  exit('エラー:'.$e->);
+}
+?>
