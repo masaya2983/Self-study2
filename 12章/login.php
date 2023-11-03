bb8fed3ea359ebf2fa1b98140a495a0e41c23de9
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-03 05:25:41 +0000

    12-3

--- 12章/login.php
+++ 12章/login.php
@@ -42,6 +42,19 @@ if(isset($_SESSION['id'])){
   <head>
     <title>サークルサイト</title>
     <link rel="stylesheet" href="https://stackpath.bootstrap.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    
+    <style type="text/css">
+      form {
+        width: 100%;
+        max-width: 330px;
+        padding: 15px;
+        margin: auto;
+        text-align: center;
+      }
+      #name{
+        margin-bottom: -1px;
+        border-bottom-right-radius:0;
+        border-bottom-left-radius:0;
+      }
+    </style>
   </head>
 </html>
