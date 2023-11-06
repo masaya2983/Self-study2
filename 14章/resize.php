5d84f740b9f4f17b176377d8aa098bb43e1c537e
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-11-06 05:48:47 +0000

    14-4

--- 14章/resize.php
+++ 14章/resize.php
@@ -18,5 +18,16 @@ switch(exif_imagetype($_FILES['file']['tmp_name'])){
     imagecopyresampled($canvas, $image,0,0,0,0,$new_width,$new_height,$width,$height);
     imagegif($canvas,images/new_image.gif);
     break;
+    
+    case IMAGETYPE_PNG;
+    $image = imagecreatefromgif($_FILES['file']['tmp_name']);
+    imagecopyresampled($canvas,$image,0,0,0,0,$new_width, $new_height, $width, $height);
+    imagepng($canvas, 'images/new_images.png');
+    break;
+    
+    default:
+      exit();
 }
+imagedestroy($image);
+imagedestroy($canvas);
 ?>
